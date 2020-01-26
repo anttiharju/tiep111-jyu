ca3d72c22db7b464b9bdfa75877c7fbcbd7f6903
anttiharju <git@anttiharju.dev>
2020-01-26 19:07:02 +0200

    projektitiedostot uudestaan kiitos eclipse

--- src/fxKirjat/KirjatMain.java
+++ src/fxKirjat/KirjatMain.java
@@ -1,35 +1,38 @@
 package fxKirjat;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
 /**
- * @author antti
- * @version 15.1.2020
+ * @author anvemaha
+ * @version 26.1.2020
  *
  */
 public class KirjatMain extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("KirjatGUIView.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("kirjat.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * @param args ei käytössä
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("KirjatGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final KirjatGUIController kirjatCtrl = (KirjatGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("kirjat.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Kirjat");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
